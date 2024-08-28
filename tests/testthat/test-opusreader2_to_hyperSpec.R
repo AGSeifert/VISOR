c2d258248fb10396d5e78f8f6762f6c962b4dec7
Florian Gärber <florian.gaerber@uni-hamburg.de>
2024-08-28 14:08:32 +0200

    fix: Remove debugging test

--- tests/testthat/test-opusreader2_to_hyperSpec.R
+++ tests/testthat/test-opusreader2_to_hyperSpec.R
@@ -11,16 +11,16 @@ test_that("opusreader2 ➡️ hyperSpec: opus", {
   hy |> lapply(expect_s4_class, "hyperSpec")
 })
 
-test_that("opusreader2 ➡️ hyperSpec: opus-dbg", {
-  skip_on_ci()
-  skip_if_not_installed("opusreader2")
-  skip_if_not_installed("hyperSpec")
-
-  opusreader2_list <- suppressWarnings(opusreader2::read_opus(
-    file.path(suppressMessages(usethis::proj_path()), "data-raw", "opus-dbg")
-  ))
-
-  hy <- suppressWarnings(opusreader2_list |> opusreader2_to_hyperSpec())
-
-  hy |> lapply(expect_s4_class, "hyperSpec")
-})
+# test_that("opusreader2 ➡️ hyperSpec: opus-dbg", {
+#   skip_on_ci()
+#   skip_if_not_installed("opusreader2")
+#   skip_if_not_installed("hyperSpec")
+#
+#   opusreader2_list <- suppressWarnings(opusreader2::read_opus(
+#     file.path(suppressMessages(usethis::proj_path()), "data-raw", "opus-dbg")
+#   ))
+#
+#   hy <- suppressWarnings(opusreader2_list |> opusreader2_to_hyperSpec())
+#
+#   hy |> lapply(expect_s4_class, "hyperSpec")
+# })
